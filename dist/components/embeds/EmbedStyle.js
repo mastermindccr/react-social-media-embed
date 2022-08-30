dc78fb562c2da29b490204070a6eb436853fe374
tacosolidity <tacosolidity@gmail.com>
2022-08-30 17:34:00 +0800

    change facebook plugin width style

--- dist/components/embeds/EmbedStyle.js
+++ dist/components/embeds/EmbedStyle.js
@@ -32,14 +32,6 @@ const EmbedStyle = (_a) => {
           margin: 0 !important;
           padding: 0 !important;
         }
-
-        .rsme-embed.rsme-facebook-embed .fb-post iframe {
-          width: 100% !important;
-        }
-
-        .rsme-embed.rsme-facebook-embed .fb-post span {
-          width: 100% !important;
-        }
       `));
 };
 exports.EmbedStyle = EmbedStyle;
